c606efd647b156570b5421d9d332912b1f428c96
prashant <prashant.sharma@appinventiv.com>
2023-06-28 22:13:06 +0530

    code

---
+++ Homework/question1.js
@@ -0,0 +1,37 @@
+/* in var we can redeclare the variable with the sam value*/
+
+/*
+var name="prashnat";
+console.log(name);
+var name="prashant";
+console.log(name);
+*/
+
+/* In let we can not redeclare the variable with same vales*/
+/*
+let name="shivam";
+console.log(name);
+let name="shivam";
+console.log(name);
+*/
+
+/* the scope of var function is through out the programme*/
+var name="shivam";
+function welcome(){
+
+    function hello()
+    {
+        var name="prashant";
+        console.log(name);
+    }
+    hello();
+ }
+ console.log(name);
+welcome();
+
+/* the scope of let to the block*/
+function Welcome()
+{
+    let name="prashant";
+    console.lag(name);
+}
